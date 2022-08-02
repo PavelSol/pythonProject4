634d0ee427e52b322bddb295bd4ef472e09dd578
PavelSol <110417183+PavelSol@users.noreply.github.com>
2022-08-02 16:52:36 +0300

    Создается и заполняется матрица

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 n = int(input())
 mat = []
 c = 1
@@ -7,5 +8,9 @@ for i in range(0, n):
         b.append(c)
         c += 1
     mat.append(b)
-for i in range(0, n):
-    print(*mat[i])
+mat2 = [[0]*n]*n
+print(mat2)
+mat2[0] = mat[0]
+print(mat2)
+
+
